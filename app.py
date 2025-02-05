89dca510a74ae0b4ea92b04b47a6fb6f4a2af833
SyuhadaZ <126224648+SyuhadaZ@users.noreply.github.com>
2025-02-05 16:38:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-#import pandas as pd
-#import numpy as np
+import pandas as pd
+import numpy as np
 #from sentence_transformers import SentenceTransformer
 #from sklearn.metrics.pairwise import cosine_similarity
 import openai
