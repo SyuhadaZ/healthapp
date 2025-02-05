6b8af59fc858f482ff454b7e53bd9c6abd429c67
SyuhadaZ <126224648+SyuhadaZ@users.noreply.github.com>
2025-02-05 16:26:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sentence_transformers import SentenceTransformer
+#from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import openai
 openai.api_key =  st.secrets["mykey"]
